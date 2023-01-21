24018200a690d7f895ff3b83f953d590c35b7db2
Syed Salman <syedsalman1006@gmail.com>
2023-01-21 15:53:15 +0530

    adding a page not found error

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import SignUp from './components/signup/SignUp';
 import UserState from './context/UserContext/UserState';
 import NewPost from './components/NewPost/NewPost';
 import Header from './components/header/header';
+import LoginMessage from './components/LoginMessage/LoginMessage';
 
 function App() {
 
@@ -14,10 +15,11 @@ function App() {
             <BrowserRouter>
             <Header/>
                 <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route path="/posts" element={<PostView />} />
-                    <Route path="/newpost" element={<NewPost/>}/>
+                    <Route exact path="/" element={<Login />} />
+                    <Route exact path="/signup" element={<SignUp />} />
+                    <Route exact path="/posts" element={<PostView />} />
+                    <Route exact path="/newpost" element={<NewPost/>}/>
+                    <Route exact path="/*" element={<LoginMessage/>}/>
                 </Routes>
             </BrowserRouter>
         </UserState>
